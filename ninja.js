57ef0db34fcb1e1c275cb3d81d714fc680f53016
JavierVSM <javier.vsm@gmail.com>
2021-11-24 11:41:47 -0600

    First Commit

---
+++ ninja.js
@@ -0,0 +1,52 @@
+class Ninja{
+    constructor(name) {
+        this.name = name;
+        this.health = 100;
+        this.speed=3;
+        this.strength=3;
+        }
+        sayName() {
+            console.log("My ninja name is " + this.name + "!");
+        }
+        showStats() {
+            console.log("Name: "+ this.name + ", Health: " + this.health + ", Speed: " + this.speed + ", Strength: " + this.strength);
+        }
+        drinkSake() {
+          this.health = this.health+10;
+        }
+}
+//TEST
+
+let steve = new Ninja("Steve");
+steve.sayName();
+steve.showStats();
+steve.drinkSake();
+steve.showStats();
+
+let anika = new Ninja("Anika");
+anika.sayName();
+anika.showStats();
+anika.drinkSake();
+anika.showStats();
+
+//Part II
+class Sensei extends Ninja{
+    constructor(name) {
+        super (name);
+        this.health = 200;
+        this.speed=10;
+        this.strength=10;
+        this.wisdom=10;
+        }
+        speakWisdom () {
+            super.drinkSake();
+            console.log("What one programmer can do in one month, two programmers can do in two months.");
+        }
+}
+
+//Test
+const superSensei = new Sensei("Master Splinter");
+superSensei.speakWisdom();
+// -> "What one programmer can do in one month, two programmers can do in two months."copy
+superSensei.showStats();
+// -> "Name: Master Splinter, Health: 210, Speed: 10, Strength: 10"
